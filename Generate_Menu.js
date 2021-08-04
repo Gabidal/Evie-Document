2718a1a52a23378697b79b8828e37608d5720d9a
Gabidal <Quanfoxes@gmail.com>
2021-08-04 23:13:15 +0300

    .

---
+++ Generate_Menu.js
@@ -0,0 +1,21 @@
+const Headers = ["Classes", "Base types", "Functions", "If statements", "Loops", "File include", "Templates", "Preprosessor", "VTC", "Arrays", "Member functions", "Static functions", "Namespaces"]
+
+function Generate_List(){
+    const List = document.getElementById("menu")
+
+    for (const Header of Headers){
+        const Item = document.createElement("li")
+        Item.className = "Element"
+        Item.onclick = () => { 
+            document.getElementById(Header).scrollIntoView({behavior: "smooth"})
+            for (const I of List.getElementsByTagName("li")){
+                I.style.backgroundColor = "rgba(255, 255, 255, 0)"
+            }
+            Item.style.backgroundColor = "rgba(255, 255, 255, 0.255)"
+        }
+        Item.innerHTML = Header
+        
+        List.appendChild(Item)
+
+    }
+}
